60131952f183de9ba8575fbcf4a0f101baf3cf42
Mars Lee <jlee10@brockport.edu>
2019-07-18 15:25:27 -0400

    Updates

---
+++ precache-manifest.b1dab80f598fa82a150487daf76ab409.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c23edc99c9089ce6df967fb48a2b09d3",
+    "url": "/selector/index.html"
+  },
+  {
+    "revision": "7e2c0729da7c5a35a733",
+    "url": "/selector/static/css/main.d54f5471.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/selector/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "7e2c0729da7c5a35a733",
+    "url": "/selector/static/js/main.b158edbe.chunk.js"
+  },
+  {
+    "revision": "90afcdfb8efb11a1f025",
+    "url": "/selector/static/js/runtime~main.3573b4bb.js"
+  }
+]);
